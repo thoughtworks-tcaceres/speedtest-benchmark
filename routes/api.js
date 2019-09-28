bd68a9fd18a11e9a028a025a7d41f46d76a9ed69
tylerccaceres <tyler.caceres@gmail.com>
2019-09-28 17:56:38 -0400

    create html page

--- routes/api.js
+++ routes/api.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const router = express.Router();
-const { getPlayerRankingsByGameType, getArchivedGames } = require("../bin/helpers/dbHelpers");
+const { getAllUsersDB } = require("../bin/helpers/dbHelpers");
 
-router.get("/archivedGames", (req, res) => {
-  // getArchivedGames()
-  //   .then(results => res.json(results))
-  //   .catch(err => res.json({ err: err.message }));
-  console.log("hello");
+router.get("/archivedGames", async (req, res) => {
+  try {
+    const data = await getArchivedGames;
+    return res.json(data);
+  } catch (error) {
+    console.log("error :", error);
+  }
 });
 
 module.exports = router;
