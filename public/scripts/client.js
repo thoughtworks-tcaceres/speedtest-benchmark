7027e15d867948004658efe5c6ce02e3e8feb463
tylerccaceres <tyler.caceres@gmail.com>
2019-09-28 17:11:55 -0400

    attempt to fix

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,110 +1,28 @@
 let socket = io.connect("http://localhost:8080/");
-let gameRoomIdUserIsTryingToJoinWithPasscode;
 
-//socket event listeners
-socket.on("directToGame", data => {
-  $("#lobby").slideUp(100);
-  console.log("MAKI CHAN");
-  console.log(`#gameFor${data.uniqueRoomName}`);
-  $(`#gameFor${data.gameId}`).toggle(1000);
-});
+// //socket event listeners
+// socket.on("directToGame", data => {
+//   $("#lobby").slideUp(100);
+//   console.log("MAKI CHAN");
+//   console.log(`#gameFor${data.uniqueRoomName}`);
+//   $(`#gameFor${data.gameId}`).toggle(1000);
+// });
 
-// Loading jquery
+// // functions
+// const handleDeleteRoomBtn = function() {
+//   $("#deleteRoomBtn").on("click", function() {
+//     socket.emit("deleteSpecificRoom");
+//   });
+// };
 
-$(document).ready(function() {
-  roomJoiner();
-  dynamicRoom();
-  showRoomsForGame();
-  // userRedirection();
-  loadJoinGameBtn();
-  passcodeInputHandler();
-  handleCancelPasscode();
-  handleDeleteRoomBtn();
-});
-
-// To be loaded on jquery when DOM is ready
-
-const handleDeleteRoomBtn = function() {
-  $("#deleteRoomBtn").on("click", function() {
-    socket.emit("deleteSpecificRoom");
-  });
-};
-
-const handleCancelPasscode = function() {
-  $("#passcodeCancel").on("click", function() {
-    $("#passcodeForRoom").hide();
-    $("#lobby").toggle(500);
-  });
-};
-
-const loadJoinGameBtn = function() {
-  $("#joinGameBtn").on("click", function() {
-    socket.emit("handleJoinGameEvent");
-  });
-};
-
-const roomJoiner = function() {
-  $(".room").on("click", function() {
-    // IMPORTANT //
-    // const enteredPasscode = prompt('This room is locked. Enter passcode here')
-    // console.log(enteredPasscode)
-    // console.log('ENDING MAS')
-    // return;
-    //////////////////////
-
-    socket.emit("checkPasscode", {
-      roomId: $(this).attr("data-roomid"),
-      gameId: $(this).attr("data-gameid")
-    });
-  });
-};
-
-const passcodeInputHandler = function() {
-  $("#passcodeForm").on("submit", function(event) {
-    event.preventDefault();
-    $.ajax("/games/insertPasscode", {
-      type: "POST",
-      data: $(this).serialize(),
-      dataType: "text"
-    })
-      .done(function(data) {
-        // console.log('successful passcode Insert!', data)
-
-        socket.emit("validatePasscode", [gameRoomIdUserIsTryingToJoinWithPasscode, data]);
-        // console.log(gameRoomIdUserIsTryingToJoinWithPasscode);
-      })
-      .fail(function(error) {
-        console.log("Error at passcode submission: ", error);
-      });
-  });
-};
-
-const dynamicRoom = function() {
-  $(".roomCreator").on("submit", function(event) {
-    event.preventDefault();
-    const gameId = $(this).attr("data-gamename");
-    $.ajax("/games/createRoom", {
-      type: "POST",
-      data: $(this).serialize(),
-      dataType: "text"
-    })
-      .done(function(data) {
-        console.log("successful creation");
-        socket.emit("createNewRoom", {
-          roomId: data,
-          gameId: gameId,
-          passcode: $(`#chosenPasscodeFor${gameId}`).val()
-        });
-      })
-      .fail(function(error) {
-        console.log("Ajax failed: ", error);
-      });
-  });
-};
-
-const showRoomsForGame = function() {
-  $(".game").on("click", function(event) {
-    $(".roomList").hide();
-    $(`#${$(this).attr("data-gamename")}`).toggle(500);
-  });
-};
+// // Loading jquery
+// $(document).ready(function() {
+//   roomJoiner();
+//   dynamicRoom();
+//   showRoomsForGame();
+//   // userRedirection();
+//   loadJoinGameBtn();
+//   passcodeInputHandler();
+//   handleCancelPasscode();
+//   handleDeleteRoomBtn();
+// });
--- routes/api.js
+++ routes/api.js
@@ -2,16 +2,11 @@ const express = require("express");
 const router = express.Router();
 const { getPlayerRankingsByGameType, getArchivedGames } = require("../bin/helpers/dbHelpers");
 
-router.get("/archivedGames", isLoggedIn, (req, res) => {
-  getArchivedGames()
-    .then(results => res.json(results))
-    .catch(err => res.json({ err: err.message }));
-});
-
-router.get("/rankingsByGame", isLoggedIn, (req, res) => {
-  getPlayerRankingsByGameType()
-    .then(results => res.json(results))
-    .catch(err => res.json({ err: err.message }));
+router.get("/archivedGames", (req, res) => {
+  // getArchivedGames()
+  //   .then(results => res.json(results))
+  //   .catch(err => res.json({ err: err.message }));
+  console.log("hello");
 });
 
 module.exports = router;
